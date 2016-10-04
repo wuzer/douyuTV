dd2573621d3bb7549b1ca18a2ee1dad9a88dc56c
Jefferson <yuanwu.zon@gmail.com>
2016-10-04 12:25:11 +0800

    tabbar

--- douyuTV/douyuTV/AppDelegate.swift
+++ douyuTV/douyuTV/AppDelegate.swift
@@ -17,8 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let frame: CGRect = UIScreen.main.bounds
         
+        window = UIWindow(frame: frame)
         
+        let tabbarContoller = UITabBarController()
+        
+//        let viewController = ViewController()
+        
+        window?.rootViewController = tabbarContoller
+        window?.makeKeyAndVisible()
         
         return true
     }
---
+++ douyuTV/douyuTV/Class/TabbarViewController/DYTabbarViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DYTabbarViewController.swift
+//  douyuTV
+//
+//  Created by Jefferson on 2016/10/4.
+//  Copyright © 2016年 Jefferson. All rights reserved.
+//
+
+import Foundation
