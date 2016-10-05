c346e6a90e768ef4a4debc7eb960eeb9ca560fb3
Jefferson <yuanwu.zon@gmail.com>
2016-10-05 19:42:26 +0800

    test

---
+++ douyuTV/douyuTV/Class/TabbarViewController/DYTabbarController.swift
@@ -0,0 +1,35 @@
+//
+//  DYTabbarController.swift
+//  douyuTV
+//
+//  Created by Jefferson on 2016/10/4.
+//  Copyright © 2016年 Jefferson. All rights reserved.
+//
+
+import UIKit
+
+class DYTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- douyuTV/douyuTV/Core/Utils/UIbarButtonItem+Extension.swift
+++ douyuTV/douyuTV/Core/Utils/UIbarButtonItem+Extension.swift
@@ -1,8 +1,8 @@
 //
-//  DYTabbarViewController.swift
+//  UIbarButtonItem+Extension.swift
 //  douyuTV
 //
-//  Created by Jefferson on 2016/10/4.
+//  Created by Jefferson on 2016/10/5.
 //  Copyright © 2016年 Jefferson. All rights reserved.
 //
 
